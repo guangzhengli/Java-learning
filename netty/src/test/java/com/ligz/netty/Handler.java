bfb0cc0506123ff5ae75864c4d3ab5fc9969fbbe
liguangzheng <380449395@qq.com>
2021-07-02 16:33:52 +0800

    [netty] feat: add reactor and handler implement

---
+++ netty/src/test/java/com/ligz/netty/Handler.java
@@ -0,0 +1,54 @@
+package com.ligz.netty;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
+final class Handler implements Runnable {
+    final SocketChannel socket;
+    final SelectionKey sk;
+    ByteBuffer input = ByteBuffer.allocate(Integer.MAX_VALUE);
+    ByteBuffer output = ByteBuffer.allocate(Integer.MAX_VALUE);
+    static final int READING = 0, SENDING = 1;
+    int state = READING;
+
+    Handler(Selector sel, SocketChannel c) throws IOException {
+        socket = c;
+        c.configureBlocking(false);
+        sk = socket.register(sel, 0); // Optionally try first read now
+        sk.attach(this);
+        sk.interestOps(SelectionKey.OP_READ);
+        sel.wakeup();
+    }
+
+    boolean inputIsComplete() { return true; }
+
+    boolean outputIsComplete() { return true; }
+
+    void process() { /* ... */ }
+
+    // class Handler continued
+    public void run() {
+        try {
+            if (state == READING) read();
+            else if (state == SENDING) send();
+        } catch (IOException ex) { /* ... */ }
+    }
+
+    void read() throws IOException {
+        socket.read(input);
+        if (inputIsComplete()) {
+            process();
+            state = SENDING;
+            // Normally also do first write now
+            sk.interestOps(SelectionKey.OP_WRITE);
+        }
+    }
+
+    void send() throws IOException {
+        socket.write(output);
+        if (outputIsComplete()) sk.cancel();
+    }
+}
